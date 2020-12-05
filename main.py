826514a52640f9e367967d1d4a895e0f3f2084a1
hsjplus <hsjplus@naver.com>
2020-12-05 02:20:15 +0000

    adding comments in the files

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+# Copyright 2020 The authors of the paper (https://arxiv.org/abs/2011.12081) 
+# The paper's title: "Tackling Domain-Specific Winograd Schemas with Knowledge-Based Reasoning and Machine Learning"
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from clyngor import ASP, solve
 import clyngor
 
